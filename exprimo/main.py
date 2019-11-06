bf5c87bd0cd21d9aa29ede79d7d7b06d96f4e2f3
Eivind Lie Andreassen <ela4991@gmail.com>
2019-11-06 10:12:19 +0100

    Only run single batch

--- exprimo/main.py
+++ exprimo/main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     graph = ComputationGraph('../nets/resnet50.json')
     device_graph = DeviceGraph.load_from_file('../device_graphs/cluster2.json')
     simulator = Simulator(graph, device_graph)
-    run_time, events = simulator.simulate(batch_size=128, batches=2, return_event_trace=True)
+    run_time, events = simulator.simulate(batch_size=128, batches=1, return_event_trace=True)
 
     plot_event_trace(events)
 
