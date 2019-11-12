4fd73bdcdb79c8909fa36a34d5a3102452c5db1c
Eivind Lie Andreassen <ela4991@gmail.com>
2019-11-12 10:54:51 +0100

    Configure for optimizing resnet50

--- exprimo/test_optimizers.py
+++ exprimo/test_optimizers.py
@@ -3,15 +3,15 @@ import json
 from exprimo import DeviceGraph, Simulator, plot_event_trace, ComputationGraph
 from exprimo.optimizers import SimulatedAnnealingOptimizer, HillClimbingOptimizer, RandomHillClimbingOptimizer
 
-batches = 4
-pipeline_batches = 2
+batches = 1
+pipeline_batches = 1
 
 # optimizer = RandomHillClimbingOptimizer(patience=100)
 # optimizer = LinearSearchOptimizer(prefix_heuristic(prefix_length=4))
 optimizer = SimulatedAnnealingOptimizer(temp_schedule=50, steps=3000, batches=batches,
                                         pipeline_batches=pipeline_batches)
 device_graph = DeviceGraph.load_from_file('../device_graphs/cluster2-reduced-memory.json')
-with open('../nets/alex_v2.json') as f:
+with open('../nets/resnet50.json') as f:
     net_string = f.read()
 
 best_net = optimizer.optimize(net_string, device_graph)
@@ -25,6 +25,7 @@ execution_time, events = simulator.simulate(batch_size=128, print_memory_usage=T
 
 plot_event_trace(events)
 
+print('\n')
 print(f'Best discovered configuration: {[layer["device"] for layer in net_dict["layers"].values()]}')
 print(f'Execution time: {execution_time:.2f}ms')
 
