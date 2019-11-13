3851cfc1f8056d154c973a4389a05a9e774c91e0
Eivind Lie Andreassen <ela4991@gmail.com>
2019-11-13 11:16:00 +0100

    Add genetic algorithm optimizer

---
+++ exprimo/optimizers/genetic_algorithm.py
@@ -0,0 +1,122 @@
+import operator
+import random
+import json
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+from exprimo.optimizers.base import BaseOptimizer
+from exprimo.optimizers.utils import generate_random_placement, evaluate_placement, apply_placement
+
+
+class GAOptimizer(BaseOptimizer):
+
+    def __init__(self, population_size=100, mutation_rate=0.01, elite_size=20, steps=500, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.population_size = population_size
+        self.mutation_rate = mutation_rate
+        self.elite_size = elite_size
+        self.steps = steps
+
+    def optimize(self, net_string, device_graph):
+        net = json.loads(net_string)
+        groups = self.create_colocation_groups(net['layers'].keys())
+
+        n_groups = len(groups)
+        n_devices = len(device_graph.devices)
+
+        def create_initial_population(population_size):
+            population = []
+
+            for i in range(population_size):
+                ind = generate_random_placement(n_groups, n_devices)
+                population.append(ind)
+
+            return population
+
+        def calculate_fitness(placement):
+            run_time = evaluate_placement(apply_placement(net_string, placement, groups), device_graph)
+            return 1 / run_time
+
+        def create_ranking(population):
+            fitness_results = {}
+            for i in range(len(population)):
+                fitness_results[i] = calculate_fitness(population[i])
+            return sorted(fitness_results.items(), key=operator.itemgetter(1), reverse=True)
+
+        def selection(ranking, elite_size):
+            selection_results = []
+            df = pd.DataFrame(np.array(ranking), columns=['index', 'fitness'])
+            df['cum_sum'] = df['fitness'].cumsum() / df['fitness'].sum()
+
+            for i in range(elite_size):
+                selection_results.append(ranking[i][0])
+
+            for i in range(len(ranking) - elite_size):
+                pick = random.random()
+
+                picked_idx = int(df[df['cum_sum'] >= pick]['index'].iloc[0])
+                selection_results.append(picked_idx)
+
+            return selection_results
+
+        def get_mating_pool(population, selection_results):
+            mating_pool = [population[idx] for idx in selection_results]
+            return mating_pool
+
+        def breed(parent1, parent2):
+            split = random.randint(0, len(parent1) - 1)
+            child = parent1[:split] + parent2[split:]
+            return child
+
+        def breed_population(mating_pool, elite_size):
+            children = []
+            breed_size = len(mating_pool) - elite_size
+            pool = random.sample(mating_pool, len(mating_pool))
+
+            for i in range(elite_size):
+                children.append(mating_pool[i])
+
+            for i in range(breed_size):
+                child = breed(pool[i], pool[-i - 1])
+                children.append(child)
+
+            return children
+
+        def mutate(individual, mutation_rate):
+            for i in range(len(individual)):
+                if random.random() < mutation_rate:
+                    individual[i] = random.randint(0, n_devices - 1)
+            return individual
+
+        def mutate_population(population, mutation_rate):
+            mutated_pop = []
+            for ind in population:
+                mutated = mutate(ind, mutation_rate)
+                mutated_pop.append(mutated)
+            return mutated_pop
+
+        def create_next_generation(current_gen, elite_size, mutation_rate):
+            pop_rank = create_ranking(current_gen)
+            selection_results = selection(pop_rank, elite_size)
+            mating_pool = get_mating_pool(current_gen, selection_results)
+            children = breed_population(mating_pool, elite_size)
+            next_gen = mutate_population(children, mutation_rate)
+
+            return next_gen
+
+        pop = create_initial_population(self.population_size)
+
+        for g in tqdm(range(self.steps)):
+            pop = create_next_generation(pop, self.elite_size, self.mutation_rate)
+
+            if self.verbose and (g+1) % 50 == 0:
+                ranking = create_ranking(pop)
+                best_solution = pop[ranking[0][0]]
+                best_score = ranking[0][1]
+                print(f'[{g+1}/{self.steps}] Best time: {1 / best_score:.2f}ms \t Best solution: {best_solution}')
+
+        ranking = create_ranking(pop)
+        best_solution = pop[ranking[0][0]]
+        return best_solution
--- exprimo/test_optimizers.py
+++ exprimo/test_optimizers.py
@@ -2,18 +2,20 @@ import json
 
 from exprimo import DeviceGraph, Simulator, plot_event_trace, ComputationGraph
 from exprimo.optimizers import SimulatedAnnealingOptimizer, HillClimbingOptimizer, RandomHillClimbingOptimizer, \
-    exponential_multiplicative_decay
+    GAOptimizer, exponential_multiplicative_decay
 
 batches = 1
 pipeline_batches = 1
 
 # optimizer = RandomHillClimbingOptimizer(patience=100)
 # optimizer = LinearSearchOptimizer(prefix_heuristic(prefix_length=4))
-optimizer = SimulatedAnnealingOptimizer(temp_schedule=exponential_multiplicative_decay(50, 0.98),
-                                        steps=30000, batches=batches,
-                                        pipeline_batches=pipeline_batches, verbose=True)
+# optimizer = SimulatedAnnealingOptimizer(temp_schedule=exponential_multiplicative_decay(50, 0.98),
+#                                         steps=30000, batches=batches,
+#                                         pipeline_batches=pipeline_batches, verbose=True)
+optimizer = GAOptimizer(population_size=100, mutation_rate=0.01, elite_size=20, steps=500, verbose=True)
+
 device_graph = DeviceGraph.load_from_file('../device_graphs/cluster2-reduced-memory.json')
-with open('../nets/resnet50.json') as f:
+with open('../nets/alex_v2.json') as f:
     net_string = f.read()
 
 best_net = optimizer.optimize(net_string, device_graph)
