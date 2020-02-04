96a27c32207868c363798e55c306b74457a03fbc
Eivind Lie Andreassen <ela4991@gmail.com>
2020-02-04 14:59:32 +0100

    .

--- exprimo/test_optimizers.py
+++ exprimo/test_optimizers.py
@@ -14,17 +14,17 @@ pipeline_batches = 1
 # optimizer = SimulatedAnnealingOptimizer(temp_schedule=exponential_multiplicative_decay(50, 0.98),
 #                                         steps=30000, batches=batches,
 #                                         pipeline_batches=pipeline_batches, verbose=True)
-# optimizer = GAOptimizer(population_size=50, mutation_rate=0.05, elite_size=10, steps=500,
-#                         early_stopping_threshold=None, verbose=False,
-#                         use_caching=True,
-#                         colocation_heuristic=prefix_heuristic(prefix_length=5))
-optimizer = GAIndirectOptimizer(population_size=50, mutation_rate=0.05, elite_size=10, steps=500,
-                                verbose=False,
-                                use_caching=True,
-                                colocation_heuristic=prefix_heuristic(prefix_length=5))
+optimizer = GAOptimizer(population_size=50, mutation_rate=0.05, elite_size=10, steps=500,
+                        early_stopping_threshold=None, verbose=False,
+                        use_caching=True)
+                        # colocation_heuristic=prefix_heuristic(prefix_length=5))
+# optimizer = GAIndirectOptimizer(population_size=50, mutation_rate=0.05, elite_size=10, steps=500,
+#                                 verbose=False,
+#                                 use_caching=True,
+#                                 colocation_heuristic=prefix_heuristic(prefix_length=5))
 
-device_graph = DeviceGraph.load_from_file('../device_graphs/cluster2-reduced-alex.json')
-with open('../nets/alex_v2.json') as f:
+device_graph = DeviceGraph.load_from_file('../device_graphs/cluster2.json')
+with open('../nets/inception_v3.json') as f:
     net_string = f.read()
 
 best_net = optimizer.optimize(net_string, device_graph)
@@ -39,6 +39,6 @@ execution_time, events = simulator.simulate(batch_size=128, print_memory_usage=T
 plot_event_trace(events)
 
 print('\n')
-print(f'Best discovered configuration: {[layer["device"] for layer in net_dict["layers"].values()]}')
+# print(f'Best discovered configuration: {[layer["device"] for layer in net_dict["layers"].values()]}')
 print(f'Execution time: {execution_time:.2f}ms')
 
