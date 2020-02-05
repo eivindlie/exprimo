c03fa06cc526940a54fded138da5be9782990536
Eivind Lie Andreassen <ela4991@gmail.com>
2020-02-05 13:12:53 +0100

    Fix ylim on memory plot

--- exprimo/plotting.py
+++ exprimo/plotting.py
@@ -31,10 +31,11 @@ def plot_event_trace(events, simulator, show_transfer_lines=True, show_memory_us
         plot_height = 1 / (len(devices) + 1)
         for d, device in enumerate(devices):
             # max_memory = simulator.device_graph.devices[device].device.memory * 10**9
-            ax = gnt.inset_axes((0, plot_height * (d + 1) - 0.005, 1, plot_height / 2), sharex=gnt, zorder=1)
+            ax = gnt.inset_axes((0, plot_height * (d + 1), 1, plot_height / 2), sharex=gnt, zorder=1)
             # ax.set_ylim(0, max_memory)
             ax.axis('off')
             ax.fill_between(memory_x, memory_y[device], facecolor='grey', alpha=0.2)
+            ax.set(ylim=(0, None))
 
     cmap_func = matplotlib.cm.get_cmap(cmap, 2*batches)
     cmap_norm = Normalize(vmin=0, vmax=2*batches)
