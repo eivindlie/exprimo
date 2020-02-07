b21f0bd106b0322617b9df13a1b37136c58b2d96
Eivind Lie Andreassen <ela4991@gmail.com>
2020-02-07 14:00:16 +0100

    Fix wrong call to plot_event_trace

--- exprimo/test_optimizers.py
+++ exprimo/test_optimizers.py
@@ -24,7 +24,7 @@ optimizer = GAOptimizer(population_size=50, mutation_rate=0.05, elite_size=10, s
 #                                 colocation_heuristic=prefix_heuristic(prefix_length=5))
 
 device_graph = DeviceGraph.load_from_file('../device_graphs/cluster2.json')
-with open('../nets/inception_v3.json') as f:
+with open('../nets/resnet50.json') as f:
     net_string = f.read()
 
 best_net = optimizer.optimize(net_string, device_graph)
@@ -36,7 +36,7 @@ simulator = Simulator(graph, device_graph)
 execution_time, events = simulator.simulate(batch_size=128, print_memory_usage=True, print_event_trace=True,
                                             return_event_trace=True, batches=batches, pipeline_batches=pipeline_batches)
 
-plot_event_trace(events)
+plot_event_trace(events, simulator)
 
 print('\n')
 # print(f'Best discovered configuration: {[layer["device"] for layer in net_dict["layers"].values()]}')
