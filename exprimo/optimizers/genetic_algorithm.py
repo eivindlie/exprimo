8762ed90fe03efe06e029ac15b69db5fbae78222
Eivind Lie Andreassen <ela4991@gmail.com>
2020-02-10 10:46:39 +0100

    Add structure of new GA

--- exprimo/optimizers/genetic_algorithm.py
+++ exprimo/optimizers/genetic_algorithm.py
@@ -1,158 +1,111 @@
-import operator
 import random
-import json
 
 import numpy as np
-import pandas as pd
-from tqdm import tqdm
 
 from exprimo.optimizers.base import BaseOptimizer
-from exprimo.optimizers.utils import generate_random_placement, evaluate_placement, apply_placement
 from exprimo.graph import get_flattened_layer_names
+from exprimo.optimizers.utils import evaluate_placement, apply_placement, generate_random_placement
+
+
+def _create_parent_selection_function(type, s=1):
+    if type in ('linear', 'lin'):
+        def linear(rank, n_parents):
+            i = n_parents - rank - 1
+            return ((2 - s) / n_parents) + ((2 * i * (s - 1)) / (n_parents * (n_parents - 1)))
+        return linear
+    elif type in ('exponential', 'exp'):
+        def exponential(rank, n_parents):
+            i = n_parents - rank - 1
+            return (1 - np.exp(-i)) / 1
+        return exponential
 
 
 class GAOptimizer(BaseOptimizer):
 
-    def __init__(self, population_size=100, mutation_rate=0.01, elite_size=20, steps=500,
-                 early_stopping_threshold=None, use_caching=False, crossover_type='1-point', *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.population_size = population_size
+    def __init__(self, mutation_rate=0.05, crossover_rate=0.8, crossover_type='one-point',
+                 parent_selection_function='linear', parent_selection_function_s=1,
+                 population_size=100, generations=100):
+        """
+        Initializes the GA optimizer, setting important hyperparameters.
+        :param mutation_rate: The rate at which mutation will be applied, set at the gene level.
+        :param crossover_rate: The rate at which crossover will be applied.
+        :param crossover_type: The type of crossover. ['uniform', 'one-point', 'n-point'] (n is any integer)
+        :param parent_selection_function: The type of distribution function applied during parent selection.
+                                          ['linear', 'exponential']
+        """
         self.mutation_rate = mutation_rate
-        self.elite_size = elite_size
-        self.steps = steps
-        self.early_stopping_threshold = early_stopping_threshold
-        self.use_caching = use_caching
-        self.crossover_type = crossover_type
+        self.crossover_rate = crossover_rate
+        self.population_size = population_size
+
+        if crossover_type == 'uniform':
+            self.crossover = 'uniform'
+        elif crossover_type == 'one-point':
+            self.crossover = 1
+        elif crossover_type.endswith('-point'):
+            self.crossover = int(crossover_type.split('-')[0])
+        else:
+            raise Exception('Invalid crossover type.')
+
+        self.parent_selection_function = _create_parent_selection_function(parent_selection_function,
+                                                                           parent_selection_function_s)
+        self.generations = generations
 
     def optimize(self, net_string, device_graph):
+        n_devices = len(device_graph.devices)
         groups = self.create_colocation_groups(get_flattened_layer_names(net_string))
 
-        n_groups = len(groups)
-        n_devices = len(device_graph.devices)
+        def initialize(population_size):
+            return [generate_random_placement(len(groups), n_devices) for i in range(population_size)]
 
-        best_score = 0
-        early_stopping_counter = 0
+        def evaluate(individual):
+            return 1 / evaluate_placement(apply_placement(net_string, individual, groups))
 
-        if self.use_caching:
-            fitness_cache = {}
+        def rank(population):
+            return sorted(population, key=evaluate)
 
-        def create_initial_population(population_size):
-            population = []
-
-            for i in range(population_size):
-                ind = generate_random_placement(n_groups, n_devices)
-                population.append(ind)
-
+        def select_parents(population):
             return population
 
-        def calculate_fitness(placement):
-            if self.use_caching:
-                placement_t = tuple(placement)
-                if placement_t in fitness_cache:
-                    return fitness_cache[placement_t]
+        def crossover(parent1, parent2):
+            if random.random() > self.crossover_rate:
+                return parent1, parent2
 
-            run_time = evaluate_placement(apply_placement(net_string, placement, groups), device_graph,
-                                          batches=self.batches, pipeline_batches=self.pipeline_batches)
+            if self.crossover == 'uniform' or crossover >= len(parent1):
+                child1, child2 = [], []
 
-            if self.use_caching:
-                fitness_cache[tuple(placement)] = 1 / run_time
-
-            return 1 / run_time
-
-        def create_ranking(population):
-            fitness_results = {}
-            for i in range(len(population)):
-                fitness_results[i] = calculate_fitness(population[i])
-            return sorted(fitness_results.items(), key=operator.itemgetter(1), reverse=True)
-
-        def selection(ranking, elite_size):
-            selection_results = []
-            df = pd.DataFrame(np.array(ranking), columns=['index', 'fitness'])
-            df['cum_sum'] = df['fitness'].cumsum() / df['fitness'].sum()
-
-            for i in range(elite_size):
-                selection_results.append(ranking[i][0])
-
-            for i in range(len(ranking) - elite_size):
-                pick = random.random()
-
-                picked_idx = int(df[df['cum_sum'] >= pick]['index'].iloc[0])
-                selection_results.append(picked_idx)
-
-            return selection_results
-
-        def get_mating_pool(population, selection_results):
-            mating_pool = [population[idx] for idx in selection_results]
-            return mating_pool
-
-        def breed(parent1, parent2):
-            if self.crossover_type == '1-point':
-                split = random.randint(0, len(parent1) - 1)
-                child = parent1[:split] + parent2[split:]
-            elif self.crossover_type == 'uniform':
-                child = []
-                for i in range(len(parent1)):
+                for g in range(len(parent1)):
                     if random.random() > 0.5:
-                        child.append(parent1[i])
+                        child1.append(parent1[g])
+                        child2.append(parent2[g])
                     else:
-                        child.append(parent1[i])
-            return child
+                        child2.append(parent1[g])
+                        child1.append(parent2[g])
+            else:
+                pass
 
-        def breed_population(mating_pool, elite_size):
+        def recombine(mating_pool):
+            assert len(mating_pool) % 2 == 0, "Mating pool must contain an equal number of parents"
+            mating_pool = random.shuffle(mating_pool)
             children = []
-            breed_size = len(mating_pool) - elite_size
-            pool = random.sample(mating_pool, len(mating_pool))
-
-            for i in range(elite_size):
-                children.append(mating_pool[i])
-
-            for i in range(breed_size):
-                child = breed(pool[i], pool[-i - 1])
-                children.append(child)
-
+            for i in range(0, len(mating_pool), 2):
+                children.extend(crossover(mating_pool[i], mating_pool[i + 1]))
             return children
 
-        def mutate(individual, mutation_rate):
-            for i in range(len(individual)):
-                if random.random() < mutation_rate:
-                    individual[i] = random.randint(0, n_devices - 1)
-            return individual
+        def mutate(individual):
+            return [random.randint(0, n_devices - 1) if random.random() < self.mutation_rate else g
+                    for g in individual]
 
-        def mutate_population(population, mutation_rate):
-            mutated_pop = []
-            for ind in population:
-                mutated = mutate(ind, mutation_rate)
-                mutated_pop.append(mutated)
-            return mutated_pop
+        def mutate_population(population):
+            return [mutate(ind) for ind in population]
 
+        def select_offspring(previous_generation, candidates):
+            return candidates
 
-        pop = create_initial_population(self.population_size)
+        pop = initialize(self.population_size)
 
-        for g in tqdm(range(self.steps)):
-            pop_rank = create_ranking(pop)
-
-            if pop_rank[0][1] > best_score:
-                best_score = pop_rank[0][1]
-                early_stopping_counter = 0
-            else:
-                early_stopping_counter += 1
-
-            selection_results = selection(pop_rank, self.elite_size)
-            mating_pool = get_mating_pool(pop, selection_results)
-            children = breed_population(mating_pool, self.elite_size)
-            next_gen = mutate_population(children, self.mutation_rate)
-
-            pop = next_gen
-
-            if self.early_stopping_threshold and early_stopping_counter > self.early_stopping_threshold:
-                print('Early stopping criterion achieved. Stopping training.')
-                break
-
-            if self.verbose and (g+1) % 10 == 0:
-                best_solution = pop[pop_rank[0][0]]
-                best_score = pop_rank[0][1]
-                print(f'[{g+1}/{self.steps}] Best time: {1 / best_score:.2f}ms \t Best solution: {best_solution}')
-
-        ranking = create_ranking(pop)
-        best_solution = pop[ranking[0][0]]
-        return json.dumps(apply_placement(net_string, best_solution, groups))
+        for i in range(self.generations):
+            ranked_pop = rank(pop)
+            mating_pool = select_parents(ranked_pop)
+            children = recombine(mating_pool)
+            candidates = mutate_population(children)
+            pop = select_offspring(pop, candidates)
