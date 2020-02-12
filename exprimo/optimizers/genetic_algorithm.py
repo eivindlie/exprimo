0c56b8c8c88ba50a50c5431a311a6acb5ca6dffa
Eivind Lie Andreassen <ela4991@gmail.com>
2020-02-12 13:50:46 +0100

    Mutate mutation rate before applying it

--- exprimo/optimizers/genetic_algorithm.py
+++ exprimo/optimizers/genetic_algorithm.py
@@ -193,11 +193,12 @@ class GAOptimizer(BaseOptimizer):
         def mutate(individual):
             if self.evolve_mutation_rate:
                 mutation_rate = individual.mutation_rate
-                placement = individual.placement
-                placement = [random.randint(0, n_devices - 1) if random.random() < mutation_rate else g
-                             for g in placement]
                 new_mutation_rate = max(min(mutation_rate + random.normalvariate(0, 0.05), self.max_mutation_rate),
                                         self.min_mutation_rate)
+                placement = individual.placement
+                placement = [random.randint(0, n_devices - 1) if random.random() < new_mutation_rate else g
+                             for g in placement]
+
                 return Candidate(placement, new_mutation_rate)
             else:
                 placement = [random.randint(0, n_devices - 1) if random.random() < self.mutation_rate else g
