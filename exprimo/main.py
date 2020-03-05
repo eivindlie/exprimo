dedb132c655b73a553a0f1eb468356459981b9fa
Eivind Lie Andreassen <ela4991@gmail.com>
2020-03-05 12:58:03 +0100

    Create scores with double bandwidth

--- exprimo/main.py
+++ exprimo/main.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from graph import ComputationGraph
 from device import DeviceGraph
@@ -24,26 +25,43 @@ def place_all_layers_to_device(net_dict, device=1):
 
 
 if __name__ == '__main__':
-    graph_file = '../nets/resnet50.json'
-    device_file = '../device_graphs/malvik.json'
+    graph_folder = '../experiment_results/sim_real_comp/nets'
+    device_file = '../device_graphs/malvik-double-bandwidth.json'
+
+    results_file = '../experiment_results/sim_real_comp/scores_double_bandwidth.csv'
+
+    with open(results_file, 'w') as f:
+        f.write('')
+
     batches = 1
     pipeline_batches = 1
 
-    with open(graph_file) as f:
-        net_dict = json.load(f)
+    dir_list = os.listdir(graph_folder)
+    for i, file_path in enumerate(dir_list):
+        if not file_path.endswith('.json'):
+            continue
 
-    net_dict = place_all_layers_to_device(net_dict, 2)
+        print(f'Simulating assignment {i + 1}/{len(dir_list)}: {file_path}')
 
-    net_string = json.dumps(net_dict)
+        generation = int(file_path.replace('gen_', '').replace('.json', ''))
 
-    graph = ComputationGraph(force_device=None)
-    graph.load_from_string(net_string)
-    device_graph = DeviceGraph.load_from_file(device_file)
-    simulator = Simulator(graph, device_graph)
-    run_time, events = simulator.simulate(batch_size=128, batches=batches, pipeline_batches=pipeline_batches,
-                                          return_event_trace=True, print_event_trace=True)
+        with open(os.path.join(graph_folder, file_path)) as f:
+            net_dict = json.load(f)
 
-    plot_event_trace(events, simulator, show_transfer_lines=True, plot_op_time_distribution=True)
+        net_string = json.dumps(net_dict)
 
-    print()
-    print(f'Total batch run time: {run_time:,.2f}ms')
+        graph = ComputationGraph(force_device=None)
+        graph.load_from_string(net_string)
+        device_graph = DeviceGraph.load_from_file(device_file)
+        simulator = Simulator(graph, device_graph)
+        run_time, events = simulator.simulate(batch_size=128, batches=batches, pipeline_batches=pipeline_batches,
+                                              return_event_trace=True, print_event_trace=False,
+                                              print_memory_usage=False)
+
+        with open(results_file, 'a') as f:
+            f.write(f'{generation}, {run_time}\n')
+
+        # plot_event_trace(events, simulator, show_transfer_lines=True, plot_op_time_distribution=True)
+
+        print()
+        print(f'Total batch run time: {run_time:,.2f}ms')
