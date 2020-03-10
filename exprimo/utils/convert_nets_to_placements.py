423d0428149387a3605a9ab711ab0c39d64741fd
Eivind Lie Andreassen <ela4991@gmail.com>
2020-03-10 15:50:53 +0100

    Make convert_nets_to_placements handle layers without set devices (default to device 0)

--- exprimo/utils/convert_nets_to_placements.py
+++ exprimo/utils/convert_nets_to_placements.py
@@ -1,8 +1,8 @@
 import os
 import json
 
-directory = 'experiment_results/sim_real_comp/nets'
-save_directory = 'experiment_results/sim_real_comp/device_assignments'
+directory = 'nets'
+save_directory = 'nets/device_assignments'
 
 device_assignment = {}
 
@@ -14,12 +14,18 @@ for file in os.listdir(directory):
             for layer_name in net_dict['layers'].keys():
                 layer = net_dict['layers'][layer_name]
 
-                device_assignment[layer_name] = layer['device']
+                try:
+                    device_assignment[layer_name] = layer['device']
+                except KeyError:
+                    device_assignment[layer_name] = 0
 
                 if layer['type'] == 'Block':
                     for sublayer_name in layer['layers'].keys():
                         sublayer = layer['layers'][sublayer_name]
-                        device_assignment[f'{layer_name}/{sublayer_name}'] = layer['device']
+                        try:
+                            device_assignment[f'{layer_name}/{sublayer_name}'] = layer['device']
+                        except KeyError:
+                            device_assignment[f'{layer_name}/{sublayer_name}'] = 0
 
             with open(os.path.join(save_directory, file), 'w') as f:
                 json.dump(device_assignment, f, indent=4)
