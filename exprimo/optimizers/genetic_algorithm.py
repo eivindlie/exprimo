312f5dce2cd019309730b481effe5e219f690fab
Eivind Lie Andreassen <ela4991@gmail.com>
2020-03-23 13:54:14 +0100

    Fix error after closing worker pool

--- exprimo/optimizers/genetic_algorithm.py
+++ exprimo/optimizers/genetic_algorithm.py
@@ -137,7 +137,7 @@ class GAOptimizer(BaseOptimizer):
 
                 fitness_scores = list(map(benchmark, population))
             else:
-                if self.n_threads > 1:
+                if self.worker_pool:
                     fn_arg = zip(population, repeat(net_string), repeat(groups), repeat(device_graph),
                                  repeat(self.pipeline_batches), repeat(self.batches))
                     fitness_scores = self.worker_pool.starmap(_evaluate, fn_arg)
@@ -323,14 +323,15 @@ class GAOptimizer(BaseOptimizer):
         print('Optimizing with simulator...')
         run_optimization(self.generations)
 
-        self.worker_pool.close()
-        self.worker_pool = None
-
         if self.benchmarking_generations and self.benchmarking_function:
             print('Optimizing with benchmarking...')
 
             if self.benchmarking_population_size < self.population_size:
                 ranked_pop = rank(pop)
+
+                self.worker_pool.close()
+                self.worker_pool = None
+
                 pop = select_offspring(ranked_pop, pop, population_size=self.benchmarking_population_size)
 
             run_optimization(self.benchmarking_generations, benchmarking_function=self.benchmarking_function,
