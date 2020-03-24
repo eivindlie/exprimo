cb0d5f14d2ca785991d1ecf04e3158cc907870a7
Eivind Lie Andreassen <ela4991@gmail.com>
2020-03-24 16:05:26 +0100

    Set drop_batches to 1

--- exprimo/test_optimizers.py
+++ exprimo/test_optimizers.py
@@ -40,7 +40,7 @@ args = {
 
     'benchmarking_population_size': 30,
     'benchmarking_generations': 10,
-    'benchmarking_function': create_benchmark_function('resnet', verbose=True, batches=5, drop_batches=0),
+    'benchmarking_function': create_benchmark_function('resnet', verbose=True, batches=5, drop_batches=1),
 }
 # optimizer = RandomHillClimbingOptimizer(patience=100)
 # optimizer = LinearSearchOptimizer(prefix_heuristic(prefix_length=4))
