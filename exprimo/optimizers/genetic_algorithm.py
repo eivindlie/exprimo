6565e3881d3f9029f07429055600749c110e374f
Eivind Lie Andreassen <ela4991@gmail.com>
2020-04-01 12:23:05 +0200

    Revert "Only penalize memory overflow if it is positive..."
    This reverts commit 33824dbb763d8e0e1361a5236e784b698feda1b7.

--- exprimo/optimizers/genetic_algorithm.py
+++ exprimo/optimizers/genetic_algorithm.py
@@ -153,8 +153,7 @@ class GAOptimizer(BaseOptimizer):
                     if memory_overflow == -1:
                         memory_overflow = 1
 
-                    if memory_overflow > 0:
-                        time += memory_overflow * 10**9 * 1
+                    time += memory_overflow * 10**9 * 1
 
                     return 1 / time
 
