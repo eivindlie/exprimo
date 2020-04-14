82b839a6ca0440b5212da236ba390ee26f9a09ee
Eivind Lie Andreassen <ela4991@gmail.com>
2020-04-14 14:29:36 +0200

    Use whitegrid for best scores plot

--- exprimo/utils/plot_best_scores.py
+++ exprimo/utils/plot_best_scores.py
@@ -2,11 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
+sns.set_style('whitegrid')
 
-results_file = '../../experiment_results/2020-03-24_malvik_resnet_1500sim_100real.csv'
-plot_title = f'ResNet on Malvik - 500 simulated, 100 benchmarked generations'
+results_file = '../../experiment_results/2020-04-07_malvik_resnet_200sim_100real_limited-memory.csv'
+plot_title = f'ResNet on Malvik - 200 simulated, 100 benchmarked generations\nLimited memory'
 
-benchmark_limit = 1500
+benchmark_limit = 200
 
 data = pd.read_csv(results_file, index_col=0, skiprows=0, names=['generation', 'batch_time'])
 
