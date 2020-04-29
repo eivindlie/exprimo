ad8b528f6d83266179053dfb883d474258a6a334
Eivind Lie Andreassen <ela4991@gmail.com>
2020-04-29 11:43:27 +0200

    Save score plot to file

--- exprimo/utils/plot_best_scores.py
+++ exprimo/utils/plot_best_scores.py
@@ -1,15 +1,20 @@
+import os
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set()
-sns.set_style('whitegrid')
 
-results_file = '../../experiment_results/2020-04-07_malvik_resnet_200sim_100real_limited-memory.csv'
+from exprimo import PLOT_STYLE
+
+sns.set(style=PLOT_STYLE)
+
+results_file = '~/logs/e4_solution_transfer/2020-04-07_malvik_resnet_200sim_100real_limited-memory.csv'
+plot_file = '~/logs/e4_solution_transfer/2020-04-07_malvik_resnet_200sim_100real_limited-memory.svg'
 plot_title = f'ResNet on Malvik - 200 simulated, 100 benchmarked generations\nLimited memory'
 
 benchmark_limit = 200
 
-data = pd.read_csv(results_file, index_col=0, skiprows=0, names=['generation', 'batch_time'])
+data = pd.read_csv(os.path.expanduser(results_file), index_col=0, skiprows=0, names=['generation', 'batch_time'])
 
 data.plot(legend=None)
 
@@ -19,5 +24,8 @@ if benchmark_limit:
 plt.title(plot_title)
 plt.xlabel('Generation')
 plt.ylabel('Batch training time (ms)')
+
+plt.savefig(os.path.expanduser(plot_file), bb_inches='tight')
+
 plt.show()
 
