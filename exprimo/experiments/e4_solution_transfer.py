106f2c204397873aee2f3d9af7a0fcbf6a1d26d5
Eivind Lie Andreassen <ela4991@gmail.com>
2020-05-02 14:19:31 +0200

    Fix wrong config path

--- exprimo/experiments/e4_solution_transfer.py
+++ exprimo/experiments/e4_solution_transfer.py
@@ -1,6 +1,6 @@
 from exprimo.optimize import optimize_with_config
 
 
-config_path = 'configs/experiments/e4-e_ga-malvik-resnet50_long.json'
+config_path = 'configs/experiments/e4-4_ga-malvik-resnet50_long.json'
 
 optimize_with_config(config_path)
