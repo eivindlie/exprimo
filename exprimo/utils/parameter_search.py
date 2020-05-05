77229eaab68df65476fb28948774c384637421ba
Eivind Lie Andreassen <ela4991@gmail.com>
2020-05-05 15:02:48 +0200

    Standardize function evaluations for GA in parameter search

--- exprimo/utils/parameter_search.py
+++ exprimo/utils/parameter_search.py
@@ -37,8 +37,14 @@ def do_parameter_search(config_path, parameter_grid, repeats=10, verbose=False):
         if verbose:
             print(f'Testing combination {i}: {combination}...')
 
+        if 'generations' in args:
+            original_pop = args['population_size']
+
         args = args_blueprint.copy()
         args = dict(tuple(args.items()) + combination)
+
+        if 'generations' in args:
+            args['generations'] = args['generations'] * (original_pop / args['population_size'])
 
         current_config = config.copy()
         current_config['optimizer_args'] = args
