860a29279272584dcd73fc54010b271cea7d17b6
Eivind Lie Andreassen <ela4991@gmail.com>
2020-05-05 11:26:13 +0200

    Make sure log dir is set in E4

--- exprimo/experiments/e4_solution_transfer.py
+++ exprimo/experiments/e4_solution_transfer.py
@@ -3,4 +3,4 @@ from exprimo.optimize import optimize_with_config
 
 config_path = 'configs/experiments/e4-4_ga-malvik-resnet50_long.json'
 
-optimize_with_config(config_path)
+optimize_with_config(config_path, set_log_dir=True)
