1115931d0cd8ba90c54f61057471e87280b7f889
Eivind Lie Andreassen <ela4991@gmail.com>
2020-05-08 15:24:12 +0200

    Complete script for E5

--- exprimo/experiments/e5_sim_real_comp.py
+++ exprimo/experiments/e5_sim_real_comp.py
@@ -4,9 +4,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from exprimo import PLOT_STYLE, get_log_dir
+from exprimo.benchmarking.benchmark import benchmark_all_placements
+from exprimo import PLOT_STYLE, get_log_dir, optimize_with_config
+from exprimo.utils.convert_nets_to_placements import convert_to_placement
 
 sns.set(style=PLOT_STYLE)
+
+
+config_path = 'configs/experiments/e5_ga-malvik-alexnet.json'
+model_type = 'alexnet'
+
+BATCHES = 20
+
+
+def run_experiment():
+    optimize_with_config(config_path, set_log_dir=True)
+
+    convert_to_placement(os.path.join(get_log_dir(), 'checkpoints'),
+                         os.path.join(get_log_dir(), 'checkpoints', 'placements'))
+
+    benchmark_all_placements(os.path.join(get_log_dir(), 'checkpoints', 'placements'),
+                             os.path.join(get_log_dir(), 'batch_times.csv'),
+                             model_type, batches=BATCHES, drop_batches=1, format='long')
 
 
 def plot_results(sim_path, real_path):
@@ -31,5 +50,7 @@ def plot_results(sim_path, real_path):
 
 
 if __name__ == '__main__':
-    plot_results(os.path.expanduser('~/logs/e5-2/time_history.csv'),
-                 os.path.expanduser('~/logs/e5-2/batch_times_long2.csv'))
+    run_experiment()
+
+    plot_results(os.path.join(get_log_dir(), 'checkpoints', 'scores.csv'),
+                 os.path.join(get_log_dir(), 'batch_times.csv'))
