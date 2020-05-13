3d98f948244985c77dd253e08be7edb0413f39bc
Eivind Lie Andreassen <ela4991@gmail.com>
2020-05-13 15:13:51 +0200

    Set up E4 for a regular transfer of ResNet-50

--- exprimo/experiments/e4_solution_transfer.py
+++ exprimo/experiments/e4_solution_transfer.py
@@ -1,7 +1,12 @@
+import sys
+
 from exprimo.optimize import optimize_with_config
 
 
-config_path = 'configs/experiments/e4_ga-malvik-resnet50_long.json'
+config_path = 'configs/experiments/e4_ga-malvik-resnet50.json'
 
 if __name__ == '__main__':
+    if len(sys.argv):
+        config_path = sys.argv[0]
+
     optimize_with_config(config_path, set_log_dir=True)
