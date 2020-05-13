da842bd3658f5f05556ce4b79d1077ae93018b56
Eivind Lie Andreassen <ela4991@gmail.com>
2020-05-13 15:15:03 +0200

    Fix logic for checking for path in E4

--- exprimo/experiments/e4_solution_transfer.py
+++ exprimo/experiments/e4_solution_transfer.py
@@ -6,7 +6,7 @@ from exprimo.optimize import optimize_with_config
 config_path = 'configs/experiments/e4_ga-malvik-resnet50.json'
 
 if __name__ == '__main__':
-    if len(sys.argv):
-        config_path = sys.argv[0]
+    if len(sys.argv) > 1:
+        config_path = sys.argv[1]
 
     optimize_with_config(config_path, set_log_dir=True)
